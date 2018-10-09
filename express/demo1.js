59fa599318ae9d6c3cb6ab47a86a8cb09c3edf55
hchen03 <hchen@qiyi.com>
2018-10-09 21:45:48 +0800

    express

---
+++ express/demo1.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+
+app.get('/', function(req, res){
+    res.send('hello world')
+})
+
+let server = app.listen(8081, function(){
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
