92a974d54331dce29430e1ebfa11b894f39505bb
hchen03 <hchen@qiyi.com>
2018-10-21 23:06:34 +0800

    comment

--- express/blog/models/posts.js
+++ express/blog/models/posts.js
@@ -1,11 +1,55 @@
 const Post = require('../lib/mongo').Post
 const marked = require('marked')
 
+const CommentModel = require('./comments')
+
+Post.plugin('addCommentsCount', {
+  afterFind: function (posts) {
+    return Promise.all(posts.map(function (post) {
+      return CommentModel.getCommentsCount(post._id).then(function (commentsCount) {
+        post.commentsCount = commentsCount
+        return post
+      })
+    }))
+  },
+  afterFindOne: function (post) {
+    if (post) {
+      return CommentModel.getCommentsCount(post._id).then(function (count) {
+        post.commentsCount = count
+        return post
+      })
+    }
+    return post
+  }
+})
+
+// 给 post 添加留言数 commentsCount
+Post.plugin('addCommentsCount', {
+  afterFind: function (posts) {
+    return Promise.all(posts.map(function (post) {
+      return CommentModel.getCommentsCount(post._id).then(function (commentsCount) {
+        post.commentsCount = commentsCount
+        return post
+      })
+    }))
+  },
+  afterFindOne: function (post) {
+    if (post) {
+      return CommentModel.getCommentsCount(post._id).then(function (count) {
+        post.commentsCount = count
+        return post
+      })
+    }
+    return post
+  }
+})
+
 Post.plugin('contentToHtml', {
   afterFind: function (posts) {
-    return posts.map(function (post) {
+    posts.map(function (post) {
       post.content = marked(post.content)
     })
+    return posts
   },
   afterFindOne: function (post) {
     if (post) {
@@ -25,6 +69,7 @@ module.exports = {
       .findOne({ _id: postId })
       .populate({ path: 'author', model: 'User' })
       .addCreatedAt()
+      .addCommentsCount()
       .contentToHtml()
       .exec()
   },
@@ -40,6 +85,7 @@ module.exports = {
       .populate({ path: 'author', model: 'User' })
       .sort({ _id: -1 })
       .addCreatedAt()
+      .addCommentsCount()
       .contentToHtml()
       .exec()
   },
@@ -62,7 +108,14 @@ module.exports = {
   },
 
   // 通过文章 id 删除一篇文章
-  delPostById: function delPostById (postId) {
-    return Post.deleteOne({ _id: postId }).exec()
+  delPostById: function delPostById (postId, author) {
+    return Post.deleteOne({ author: author, _id: postId })
+      .exec()
+      .then(function (res) {
+        // 文章删除后，再删除该文章下的所有留言
+        if (res.result.ok && res.result.n > 0) {
+          return CommentModel.delCommentsByPostId(postId)
+        }
+      })
   }
 }
